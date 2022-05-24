3d3b03aeb43b1fdb9a01d16636b71e5b8e9284ee
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-05-24 13:53:11 -0400

    Added directory for components and created a player component - player is rendered in index

---
+++ pages/components/player.tsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const Player = () => {
+  //hooks
+  return (
+  <div id="audio-player-container">
+    <audio controls></audio>
+  </div>
+  )
+}
+
+export default Player;

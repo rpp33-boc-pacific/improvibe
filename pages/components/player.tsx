384346ec1ae1a980f7ed56ef96b31d7250b043cb
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-05-24 22:09:17 -0400

    Removed 'card' component

--- pages/components/player.tsx
+++ pages/components/player.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Card from '@mui/material/card';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';

5cea08a9d2c1907fe02bbb8d73892a146c55a144
J.P. Doyle <jp.hackreactor@gmail.com>
2022-05-27 16:52:59 -0700

    Switched profiles to server-side rendering

--- pages/profiles/[name].tsx
+++ pages/profiles/[name].tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Link from 'next/link';
 import useSWR from 'swr';
 import { Box } from '@mui/material';
 import { Grid } from '@mui/material';
@@ -6,35 +7,23 @@ import { Link } from '@mui/material';
 import { Typography } from '@mui/material';
 import { CircularProgress } from '@mui/material';
 import { Alert } from '@mui/material';
-import About from '../../components/profile/About';
-import Photo from '../../components/profile/Photo';
 import Player from '../../components/reusable/AudioPlayer';
+import Photo from '../../components/profile/Photo';
+import About from '../../components/profile/About';
+import SongTiles from '../../components/profile/SongTiles';
 // import SearchBar from '../../components/SearchBar';
-import { getAllUserNames } from '../../database/profile-helpers';
 
-const getStaticPaths = () => {
-  const paths = await getAllUserNames();
-  return {
-    // paths,
-    // TO DO - delete below
-    paths: [
-      { params: { name: 'jp' } }
-    ],
-    // TO DO - delete above
-    fallback: 'blocking'
-  };
-};
-
-const getStaticProps = ({ params }) => {
-  const name = params.name;
-  return { props: { name } };
+const getServerSideProps = (context) => {
+  const name = context.params.name;
+  const owner = context.query.owner;
+  return { props: { name, owner } };
 };
 
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
-const Profile = ({ name }) => {
+const Profile = ({ name, owner }) => {
 
-  const { data, error } = useSWR(`/api/profile/${name}`, fetcher);
+  const { data, error } = useSWR(`/api/profiles/${name}`, fetcher);
   if (error) {
     return <Alert severity='error'>Error fetching data</Alert>;
   }
@@ -42,42 +31,75 @@ const Profile = ({ name }) => {
     return <CircularProgress />;
   }
 
-  return (
-    <Grid>
-      <SearchBar />
-      <Grid container>
-        <Grid item>
-          <Photo url={data.url}></Photo>
-        </Grid>
-        <Grid item>
-          <Grid container>
-            <Grid item>
-              <Typography>{name}</Typography>
-            </Grid>
-            <Grid item>
-              <Typography>Edit Profile</Typography>
+  if (owner) {
+    return (
+      <Grid>
+        <SearchBar />
+        <Grid container>
+          <Grid item>
+            <Photo photoUrl={data.user.photoUrl}></Photo>
+          </Grid>
+          <Grid item>
+            <Grid container>
+              <Grid item>
+                <Typography>{name}</Typography>
+              </Grid>
+              <Grid item>
+                <Link href={`/profileEditors/${name}`}><Typography><a>Edit Profile</a></Typography></Link>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
-        <Grid item>
-          <Typography>About Me</Typography>
-        </Grid>
-        <Grid item>
-          <About about_me={data.about_me}/>
-        </Grid>
-        <Grid item>
-          <Typography>My Songs</Typography>
-        </Grid>
-        <Grid item>
-          <Grid container>
-            <SongTiles songs={data.songs} />
+          <Grid item>
+            <Typography>About Me</Typography>
+          </Grid>
+          <Grid item>
+            <About about={data.user.about}/>
+          </Grid>
+          <Grid item>
+            <Typography>My Songs</Typography>
+          </Grid>
+          <Grid item>
+            <Grid container>
+              <SongTiles songs={data.songs} user={data.user} />
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
-  );
+    );
+  } else {
+    return (
+      <Grid>
+        <SearchBar />
+        <Grid container>
+          <Grid item>
+            <Photo photoUrl={data.user.photoUrl}></Photo>
+          </Grid>
+          <Grid item>
+            <Grid container>
+              <Grid item>
+                <Typography>{name}</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography>About Me</Typography>
+          </Grid>
+          <Grid item>
+            <About about={data.user.about}/>
+          </Grid>
+          <Grid item>
+            <Typography>My Songs</Typography>
+          </Grid>
+          <Grid item>
+            <Grid container>
+              <SongTiles songs={data.songs} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
 };
 
-export { getStaticPaths };
-export { getStaticProps };
+export { getServerSideProps };
 export default Profile;

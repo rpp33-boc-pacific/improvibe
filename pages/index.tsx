aebd5a429d44eab373605e9f7a70a98fd6575dce
jakobtruong@gmail.com <jakobtruong@gmail.com>
2022-05-26 19:10:54 -0700

    Refactored import statement due to linter

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import SearchAppBar from '../components/SearchBar.tsx';
+import SearchAppBar from '../components/SearchBar';
 import Player from '../components/reusable/AudioPlayer';
 
 import LikeButton from '../components/reusable/LikeButton';

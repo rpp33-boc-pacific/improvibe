0d893ba02b41fd16b4fc9704b336f160f29dc015
Jeff Sparrow <jeffsparrow.dev@gmail.com>
2022-05-26 21:37:44 -0500

    add button to AddLayer component

--- components/projectEditor/AddLayer.tsx
+++ components/projectEditor/AddLayer.tsx
@@ -1,7 +1,13 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 function AddLayer() {
 
   return (
-    <div>AddLayerButton</div>
+    <Stack spacing={2} direction="row">
+      <Button variant="outlined">Add Layer</Button>
+    </Stack>
   );
 }
 

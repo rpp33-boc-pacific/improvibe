062b4dbb058780c08e338093dffecb716bb8b671
Jeff Sparrow <jeffsparrow.dev@gmail.com>
2022-05-26 21:35:19 -0500

    update page structure to include projects directory with index and [id] files

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import LayerList from '../components/projectEditor/LayerList'
-import ProjectHeader from '../components/projectEditor/ProjectHeader'
-import AddLayer from '../components/projectEditor/AddLayer'
-import NewProject from '../components/projectEditor/NewProject'
-import ProjectList from '../components/projectEditor/ProjectList'
+import LayerList from '../../components/projectEditor/LayerList'
+import ProjectHeader from '../../components/projectEditor/ProjectHeader'
+import AddLayer from '../../components/projectEditor/AddLayer'
+import NewProject from '../../components/projectEditor/NewProject'
+import ProjectList from '../../components/projectEditor/ProjectList'
 
 export default function Editor() {
   return (

c11616eeec90f3ce2668816e35361777961b73af
Bhrigu Joshi <88751642+bjoshi25@users.noreply.github.com>
2022-05-28 09:30:54 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css';
 import Player from '../components/shared/AudioPlayer';
 import SongTile from '../components/shared/SongTile';
 import SearchAppBar from '../components/SearchBar';
-import LikeButton from '../components/reusable/LikeButton';
+// import LikeButton from '../components/reusable/LikeButton';
 import AddToProjects from '../components/reusable/AddToProjects';
 
 const Home: NextPage = () => {

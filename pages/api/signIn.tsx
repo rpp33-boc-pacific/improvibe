d0a71803e6b06ea0ce94e1a45410db17b196fb47
Alysha Gilliard <alyshargilliard@gmail.com>
2022-05-28 01:10:40 -0400

    Updated error message for signing in

--- pages/api/signIn.tsx
+++ pages/api/signIn.tsx
@@ -3,10 +3,6 @@ import hash from 'object-hash';
 import { useSession, getSession } from "next-auth/react"
 
 export default async function signInHandler(req: any, res: any) {
-  const session = await getSession({ req });
-
-  console.log(session)
-
   const hashedPassword = hash(req.body.password);
   const email = req.body.email;
 
@@ -15,13 +11,13 @@ export default async function signInHandler(req: any, res: any) {
   client.query(checkUserCredentials)
   .then((user) => {
     if (user.rowCount === 0) {
-      throw new Error('Invalid email or password', {cause: 'Your email or password is incorrect.'});
+      throw new Error('Invalid email or password');
     } else {
       res.send(200);
     }
   }).catch((err) => {
-    if (err.cause === 'Your email or password is incorrect.') {
-      res.send(400, err.cause);
+    if (err.message === 'Invalid email or password') {
+      res.status(400).send(err.message);
     } else {
       res.send(500, 'Error: please try again.');
     }

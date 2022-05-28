8343700f0d0912f7be4a8b9fd37f8980b9da659a
Alysha Gilliard <alyshargilliard@gmail.com>
2022-05-28 02:12:13 -0400

    Added placeholders for user and password database credentials

--- sql/db.tsx
+++ sql/db.tsx
@@ -2,6 +2,8 @@ import { Client } from 'pg';
 const client = new Client({
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
+  // user: process.env.PGUSER,
+  // password: process.env.PGDATABASE,
 });
 
 client.connect(err => {

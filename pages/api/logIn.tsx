75295cfd709feaf8aedf10a75e1fff6d8644cca4
Alysha Gilliard <alyshargilliard@gmail.com>
2022-05-28 16:19:46 -0400

    Added promise for login and sign in handler

--- pages/api/logIn.tsx
+++ pages/api/logIn.tsx
@@ -2,24 +2,27 @@ import pool from "../../sql/db";
 import hash from 'object-hash';
 
 export default async function loginHandler(req: any, res: any) {
-  const email = req.body.email;
-  const hashedPassword = hash({email: req.body.password});
+  return new Promise <void>(resolve => {
+    const email = req.body.email;
+    const hashedPassword = hash({email: req.body.password});
 
+    const checkUserCredentials = `SELECT email FROM users WHERE email='${email}' AND hash='${hashedPassword}'`
 
-  const checkUserCredentials = `SELECT email FROM users WHERE email='${email}' AND hash='${hashedPassword}'`
-
-  pool.query(checkUserCredentials)
-  .then((user: any) => {
-    if (user.rowCount === 0) {
-      throw new Error('Invalid email or password');
-    } else {
-      res.redirect('/');
-    }
-  }).catch((err: any) => {
-    if (err.message === 'Invalid email or password') {
-      res.status(400).send(err.message);
-    } else {
-      res.send(500, 'Error: please try again.');
-    }
-  });
+    pool.query(checkUserCredentials)
+    .then((user: any) => {
+      if (user.rowCount === 0) {
+        throw new Error('Invalid email or password');
+      } else {
+        res.redirect('/');
+      }
+      return resolve();
+    }).catch((err: any) => {
+      if (err.message === 'Invalid email or password') {
+        res.status(400).send(err.message);
+      } else {
+        res.send(500, 'Error: please try again.');
+      }
+      return resolve();
+    });
+  })
 }

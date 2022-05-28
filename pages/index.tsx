b1fa1377d25d65869c6504eff7e621f4d4f57be3
Bhrigu <joshi.bhrigu25@gmail.com>
2022-05-28 13:53:27 -0400

    'Updated index.tsx'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,72 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import Player from '../components/shared/AudioPlayer';
-import SongTile from '../components/shared/SongTile';
-import SearchAppBar from '../components/SearchBar';
-import LikeButton from '../components/shared/LikeButton';
-import AddToProjects from '../components/shared/AddToProjects';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Dashboard from '../components/explorerPage/dashboard';
+import PopularGenres from '../components/explorerPage/popularGenres';
+import TopArtists from '../components/explorerPage/topArtists';
+import YourContributions from '../components/explorerPage/yourContributions';
+import HorizontalCarousel from '../components/explorerPage/horizontalCarousel';
+import SearchBar from '../components/SearchBar';
+import GenreData from '../components/explorerPage/genres-sampleData';
+import Artists from '../components/explorerPage/topArtists-sampleData';
+import Songs from '../components/explorerPage/topContributions-sampleData';
+import Performance from '../components/explorerPage/dashboard-sampleData';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
-const Home: NextPage = () => {
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
-
+export default function HomePage() {
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="keywords" content="music, app, audio editing, social" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <SearchAppBar/>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to improvibe!
-        </h1>
-        <SongTile />
-      </main>
-    </div>
-  )
-};
-
-export default Home;
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Item>
+            <SearchBar/>
+          </Item>
+        </Grid>
+        <Grid item xs={12}>
+          <Item>
+          <PopularGenres GenreData = {GenreData}/>
+          </Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>
+          <HorizontalCarousel/>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+            <Dashboard Performance = {Performance}/>
+          </Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>
+            <HorizontalCarousel/>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+            <YourContributions Songs = {Songs}/>
+          </Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>
+            <HorizontalCarousel/>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+            <TopArtists Artists = {Artists}/>
+          </Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}

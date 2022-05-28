4ddbf4bdbc4046c15499a22e0b1ec4bb0877b37c
Alysha Gilliard <alyshargilliard@gmail.com>
2022-05-28 01:14:50 -0400

    Removed sessions import

--- pages/api/signIn.tsx
+++ pages/api/signIn.tsx
@@ -1,6 +1,5 @@
 import client from "../../sql/db";
 import hash from 'object-hash';
-import { useSession, getSession } from "next-auth/react"
 
 export default async function signInHandler(req: any, res: any) {
   const hashedPassword = hash(req.body.password);
@@ -13,7 +12,7 @@ export default async function signInHandler(req: any, res: any) {
     if (user.rowCount === 0) {
       throw new Error('Invalid email or password');
     } else {
-      res.send(200);
+      res.redirect('/');
     }
   }).catch((err) => {
     if (err.message === 'Invalid email or password') {

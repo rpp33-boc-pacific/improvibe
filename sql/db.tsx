ba3951b144d9fdf12161d9286c7a6973c746d9c9
Alysha Gilliard <alyshargilliard@gmail.com>
2022-05-28 01:27:26 -0400

    Set placeholder for next auth

--- sql/db.tsx
+++ sql/db.tsx
@@ -6,9 +6,9 @@ const client = new Client({
 
 client.connect(err => {
   if (err) {
-    console.error('connection error', err.stack)
+    console.error('connection error', err.stack);
   } else {
-    console.log('connected')
+    console.log('connected');
   }
 });
 

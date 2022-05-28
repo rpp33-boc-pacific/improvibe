921c82a1f92c0f295194e1d7429e3adee6bbb8e6
Alysha Gilliard <alyshargilliard@gmail.com>
2022-05-28 10:10:17 -0400

    Updated parameters for database

--- sql/db.tsx
+++ sql/db.tsx
@@ -2,8 +2,8 @@ import { Client } from 'pg';
 const client = new Client({
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
-  // user: process.env.PGUSER,
-  // password: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
 });
 
 client.connect(err => {

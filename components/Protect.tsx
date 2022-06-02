77c6a0989d1b0bf20e9cd400716db18f37c9d5ef
Alysha Gilliard <alyshargilliard@gmail.com>
2022-06-02 16:25:52 -0400

    Set up page protection

--- components/Protect.tsx
+++ components/Protect.tsx
@@ -1,19 +1,21 @@
 /* eslint-disable react/display-name */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSession } from "next-auth/react";
 import { useRouter } from 'next/router';
 
 export default function Protect (WrappedComponent: any) {
 
     return function ProtectedComponent () {
+
       const router = useRouter();
       const { data: session, status } = useSession();
 
-      if (status === "authenticated") {
-        router.push('/');
-      } else {
-        return <WrappedComponent session={session} />;
-      }
+      useEffect(() => {
+        if (status === "unauthenticated" ) {
+          router.push("/logIn");
+        }
+      })
 
+      return <WrappedComponent session={session} />;
     };
   }

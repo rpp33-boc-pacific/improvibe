dc70fc6c5065823418fe25a2d82a89b1327b6719
Alysha Gilliard <alyshargilliard@gmail.com>
2022-06-02 00:00:14 -0400

    Customized login page

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,56 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import pool from "../../../sql/db";
+import hash from 'object-hash';
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "jsmith@test.com" },
+        password: {  label: "Password", type: "password" }
+      },
+      authorize(credentials: any) {
+      const email = credentials?.email ;
+      const hashedPassword = hash(credentials?.password);
+
+      const checkUserCredentials = `SELECT email FROM users WHERE email='${email}' AND hash='${hashedPassword}'`;
+      pool.query(checkUserCredentials)
+      .then((user: any) => {
+        if (user.rowCount === 0) {
+          throw new Error('Invalid email or password');
+        } else {
+          return user;
+        }
+      }).catch((err: any) => {
+        return null;
+      });
+      }
+    })
+  ],
+
+  callbacks: {
+    jwt: ({ token, user }) => {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      if (token) {
+        session.id = token.id;
+      }
+      return session;
+    }
+  },
+  secret: "test",
+  jwt: {
+    secret: "test",
+    encryption: true
+  },
+  page: {
+    signIn: '/logIn'
+  }
+
+})
--- pages/logIn.tsx
+++ pages/logIn.tsx
@@ -1,15 +1,26 @@
 import Link from 'next/link';
-import { signIn } from "next-auth/react";
+import { getCsrfToken, getSession, getProviders, signIn } from "next-auth/react";
+import React, { useState } from 'react';
 
-export default function LogIn() {
+export default function LogIn({ csrfToken, providers }) {
 
-  const login = async (e: any) => {
-    // e.preventDefault();
-    await signIn("credentials", {email: "hello", password: "password", callbackUrl: "/"});
+  const [logInError, setLogInError] = useState(false);
+
+  const credentialsLogIn = () => {
+    signIn('credentials', {email: 'email', password: 'password', redirect: false})
+    .then((status) => {
+      if (status.error) {
+        setLogInError(true);
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    });
   }
 
   return (
     <form>
+      <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
       <h1>Login</h1>
       <label>
         Email
@@ -19,7 +30,12 @@ export default function LogIn() {
         Password
         <input name="password" type="password" />
       </label>
-        <a onClick={(e) => login(e)}>Sign in</a>
+        <button type="button" onClick={() => credentialsLogIn()}>Sign in</button>
+        {logInError &&
+        <div>
+          Email or password invalid, please try again.
+        </div>
+        }
       <p>Dont have an account yet?
         <Link href="/signUp">
           <a> Sign up</a>
@@ -28,3 +44,22 @@ export default function LogIn() {
     </form>
   )
 }
+
+LogIn.getInitialProps = async (context: any) => {
+  const { req, res } = context;
+  const session = await getSession({ req });
+
+  if (session && res && session.accessToken) {
+    res.writeHead(302, {
+      Location: "/",
+    });
+    res.end();
+    return;
+  }
+
+  return {
+    session: undefined,
+    providers: await getProviders(),
+    csrfToken: await getCsrfToken(),
+  };
+};

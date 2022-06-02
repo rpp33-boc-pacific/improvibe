ac20ba17ce2f08aa9433b7c27850b726c4aee3fe
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-06-02 09:34:34 -0400

    Saved accepted changes

--- pages/signUp.tsx
+++ pages/signUp.tsx
@@ -1,23 +1,42 @@
-import Link from 'next/link';
+import pool from "../sql/db";
+import hash from 'object-hash';
 
-export default function SignUp() {
-  return (
-    <form method="post" action="/api/signUp">
-      <h1>Sign Up</h1>
-      <label>
-        Email
-        <input name="email" type="text" />
-      </label>
-      <label>
-        Password
-        <input name="password" type="password" />
-      </label>
-      <button type="submit">Sign Up</button>
-      <p>Already have an account?
-        <Link href="/logIn">
-          <a> Log In</a>
-        </Link>
-        </p>
-    </form>
-  )
+export default function signUpHandler(req: any, res: any) {
+
+  return new Promise <void>((resolve) => {
+    if (req.method !== 'POST') {
+      res.status(405).send({ message: 'Only POST requests allowed' });
+      return;
+
+    } else {
+      const email = req.body.email;
+      const hashedPassword = hash({email: req.body.password});
+
+      const checkUserExists = `SELECT email FROM users WHERE email='${email}'`
+      const insertUser = `INSERT INTO users(email, hash) VALUES ('${email}', '${hashedPassword}');`
+
+      pool.query(checkUserExists)
+      .then((user: any) => {
+          if (user.rowCount !== 0) {
+            throw new Error('User already exists with this email address');
+          } else {
+            pool.query(insertUser)
+            .then(() => {
+              res.status(201).send('Your account has successfully been created');
+              return resolve();
+            })
+            .catch((err: any) => {
+              throw new Error('Internal error');
+            });
+          }
+      }).catch((err: any) => {
+        if (err.message === 'User already exists with this email address') {
+          res.status(400).send(err.message);
+        } else {
+          res.status(500).send('Error: please try again');
+        }
+        return resolve();
+      });
+    }
+  })
 }
--- sql/db.tsx
+++ sql/db.tsx
@@ -6,24 +6,4 @@ const pool = new Pool({
   password: process.env.POSTGRES_PASSWORD,
 });
 
-<<<<<<< HEAD
-// const client = new Client({
-//   host: 'http://ec2-3-93-190-88.compute-1.amazonaws.com/',
-//   database: process.env.PGDATABASE,
-//   user: pos,
-//   password: process.env.PGPASSWORD,
-// });
-
-client.connect(err => {
-  if (err) {
-    console.error('connection error', err.stack);
-  } else {
-    console.log('connected', client);
-  }
-});
-
-export default client;
-=======
 export default pool;
->>>>>>> main
-

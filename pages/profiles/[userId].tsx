8b30d1ad86d372d4fbbfadf23039c6c2e09b35c6
J.P. Doyle <jp.hackreactor@gmail.com>
2022-06-02 20:53:48 -0700

    Modified /profiles page

--- pages/profiles/[userId].tsx
+++ pages/profiles/[userId].tsx
@@ -1,9 +1,9 @@
 import { useContext } from 'react';
 // import Context from '../AppContext';
 import Image from 'next/image';
-import Link from 'next/link';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
@@ -11,128 +11,69 @@ import Typography from '@mui/material/Typography';
 import { Stack } from '@mui/material';
 import SearchBar from '../../components/SearchBar';
 import SongTile from '../../components/shared/SongTile';
-import profile from '../../sample-data/profile'; // REMOVE LATER
+import profile from '../../sample-data/profile';
+
+const fetcher = (...args: any) => fetch(args).then((res) => res.json())
 
 const Profile: NextPage = (/*{ Component, pageProps }: AppProps*/) => {
 
-  // const userInfo = useContext(Context);
-  // const userIdInState = userInfo.user.userId;
-  // const name = userInfo.user.name;
-  // const aboutMe = userInfo.user.aboutMe;
-  // const photoUrl = userInfo.user.photoUrl;
-  // const songs = userInfo.songs;
-
   const router = useRouter();
-  const userId: string = router.query.userId as string || '1';
+  const userId = router.query.userId;
 
-  if (userId === '1' /* === userIdInState */) {
-    return (
-      <div>
-        <SearchBar />
-        <Grid container spacing={1}>
-          <Grid item xs={4}>
+  // const { data, error } = useSWR(`/api/users/${userId}`, fetcher)
+
+  // if (error) return <div>Failed to load</div>
+  // if (!data) return <div>Loading...</div>
+  return (
+    <div>
+      <SearchBar />
+      <Grid container spacing={1}>
+        <Grid item xs={4}>
+          <Box>
+            <Container>
+              <Image
+                alt='Profile picture of the arist'
+                src={profile.photoUrl /* data.photoUrl */ }
+                height={300}
+                width={300}
+                style={{ borderRadius: '90%' }}
+              />
+            </Container>
+          </Box>
+        </Grid>
+        <Grid container item xs={8}>
+          <Grid item xs={12}>
             <Box>
-              <Container>
-                <Image
-                  alt='Profile picture of the arist'
-                  src={profile.photoUrl /* photoUrl */}
-                  height={300}
-                  width={300}
-                  style={{ borderRadius: '90%' }}
-                />
-              </Container>
+              <Typography>{profile.name /* data.artist */}</Typography>
             </Box>
           </Grid>
-          <Grid container item xs={8}>
-            <Grid item xs={9}>
-              <Box>
-                <Typography>{profile.name /* name */}</Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={3}>
-              <Box>
-                <Link href={`/profileeditors/${userId}`}><Typography><a>Edit Profile</a></Typography></Link>
-              </Box>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography>About Me</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Box>
-                {profile.aboutMe /* aboutMe */}
-              </Box>
-            </Grid>
+          <Grid item xs={12}>
+            <Typography>About Me</Typography>
           </Grid>
-          <Grid item xs={4}>
-          </Grid>
-          <Grid container item xs={8}>
-            <Grid item xs={12}>
-              <Typography>My Songs</Typography>
-            </Grid>
-            <Grid container item xs={12}>
-              <Stack direction='row' spacing={1}>
-                {/* {songs.map((song, index) => <Grid item key={index}><SongTile key={index} user={user} song={song} /></Grid>)} */}
-                <SongTile />
-                <SongTile />
-                <SongTile />
-              </Stack>
-            </Grid>
-          </Grid>
-        </Grid>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <SearchBar />
-        <Grid container spacing={1}>
-          <Grid item xs={4}>
+          <Grid item xs={12}>
             <Box>
-              <Container>
-                <Image
-                  alt='Profile picture of the arist'
-                  src={profile.photoUrl /* photoUrl */}
-                  height={300}
-                  width={300}
-                  style={{ borderRadius: '90%' }}
-                />
-              </Container>
+              {profile.aboutMe /* data.aboutMe */}
             </Box>
           </Grid>
-          <Grid container item xs={8}>
-            <Grid item xs={12}>
-              <Box>
-                <Typography>{profile.name /* name */}</Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography>About Me</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Box>
-                {profile.aboutMe /* aboutMe */}
-              </Box>
-            </Grid>
+        </Grid>
+        <Grid item xs={4}>
+        </Grid>
+        <Grid container item xs={8}>
+          <Grid item xs={12}>
+            <Typography>My Songs</Typography>
           </Grid>
-          <Grid item xs={4}>
-          </Grid>
-          <Grid container item xs={8}>
-            <Grid item xs={12}>
-              <Typography>My Songs</Typography>
-            </Grid>
-            <Grid container item xs={12}>
-              <Stack direction='row' spacing={1}>
-                {/* {songs.map((song, index) => <Grid item key={index}><SongTile key={index} user={user} song={song} /></Grid>)} */}
-                <SongTile />
-                <SongTile />
-                <SongTile />
-              </Stack>
-            </Grid>
+          <Grid container item xs={12}>
+            <Stack direction='row' spacing={1}>
+              {/* {data.map((song, index) => <Grid item key={index}><SongTile key={index} user={user} song={song} /></Grid>)} */}
+              <SongTile />
+              <SongTile />
+              <SongTile />
+            </Stack>
           </Grid>
         </Grid>
-      </div>
-    );
-  }
+      </Grid>
+    </div>
+  );
 };
 
 export default Profile;

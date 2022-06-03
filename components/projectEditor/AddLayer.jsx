0a43d933fcdf6f4bb2c957e4db8d48a35228f543
Jeff Sparrow <jeffsparrow.dev@gmail.com>
2022-06-02 23:41:28 -0500

    upload file to s3

--- components/projectEditor/AddLayer.jsx
+++ components/projectEditor/AddLayer.jsx
@@ -1,7 +1,10 @@
 import { useFilePicker } from 'use-file-picker';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import axios from 'axios';
+
+const BUCKET_URL = "https://testyhyrasz1312.s3.eu-central-1.amazonaws.com/";
 
 function AddLayer() {
 
@@ -9,15 +12,45 @@ function AddLayer() {
     accept: '.mp3',
   });
 
-  const handleUpload = async (file) => {
-    uploadFile(file, config)
-        .then((data) => console.log(data))
-        .catch((err) => console.error(err))
-  };
+  const [file, setFile] = useState(null);
+
+
+  useEffect(() => {
+    setFile(filesContent[0]);
+  });
+
+  console.log('file', file);
 
   const handleClick = () => {
     openFileSelector();
   }
+
+  const uploadFile = async () => {
+    // setUploadingStatus("Uploading the file to AWS S3");
+
+    let { data } = await axios.post("/api/s3/uploadFile", {
+      name: file.name,
+      type: file.type,
+    });
+
+    console.log(data);
+
+    const url = data.url;
+    let { data: newData } = await axios.put(url, file, {
+      headers: {
+        "Content-type": file.type,
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+
+    // setUploadedFile(BUCKET_URL + file.name);
+    setFile(null);
+  };
+
+  if (file && file !== 'undefined') {
+    uploadFile();
+  }
+
 
   if (loading) {
     return <div>Loading...</div>;
@@ -26,7 +59,7 @@ function AddLayer() {
   return (
     <Stack spacing={2} direction="row">
       <Button variant="outlined" onClick={handleClick}>Add Layer</Button>
-      <input type='file'/>
+      {/* <input type='file'/> */}
     </Stack>
   );
 }

3d9f65df712d90b0480f86ea80cc92bd84e2e5e5
jakobtruong@gmail.com <jakobtruong@gmail.com>
2022-06-04 10:22:14 -0700

    Added base tests

--- tests/query/query.test.tsx
+++ tests/query/query.test.tsx
@@ -1,9 +1,5 @@
-import { render, screen } from '@testing-library/react';
-// import FilterSelect from '../../components/queryPage/FilterSelect';
-// import { useState } from 'react';
-// import SortSelect from '../../components/queryPage/SortSelect';
-// import SongResult from '../../components/queryPage/SongResult';
-// import NavigationBar from '../../components/NavigationBar';
+import { render, screen, act } from '@testing-library/react';
+import React from 'react';
 import Query from '../../pages/query/index';
 
 const userProp = {
@@ -20,37 +16,23 @@ const songProp = {
   cumulativeLikes: 234
 }
 
-describe('Components', () => {
-  // it('Renders filter select', () => {
-  //   const [filterParams, setFilterParams] = useState<string[]>([]);
-  //   render(<FilterSelect filterParams={filterParams} setFilterParams={setFilterParams}/>);
-  //   const list = screen.getByRole('filter-select');
-  //   expect(list).toBeInTheDocument();
-  // });
-
-  // it('Renders sort select', async () => {
-  //   render(<SortSelect />);
-  //   const list = screen.getByRole('sort-select');
-  //   expect(list).toBeInTheDocument();
-  // });
-
-  // it('Renders search result', async () => {
-  //   render(<SongResult song={songProp} user={userProp} />);
-  //   const list = screen.getByRole('search-result');
-  //   expect(list).toBeInTheDocument();
-  // });
-
-  // it('Renders multiple layers', async () => {
-  //   render(<NavigationBar/>);
-  //   const list = screen.getByRole('navigation-bar');
-  //   expect(list).toBeInTheDocument();
-  // });
-});
-
 describe('Query Index Page', () => {
+  it('Renders filter select', () => {
+    render(<Query/>);
+    const navigationBar = screen.getByRole('navigation-bar');
+    expect(navigationBar).toBeInTheDocument();
+  });
+
   it('Renders filter select', () => {
     render(<Query/>);
     const filterSelect = screen.getByRole('filter-select');
     expect(filterSelect).toBeInTheDocument();
   });
+
+  it('Renders filter select', () => {
+    render(<Query/>);
+    const sortSelect = screen.getByRole('sort-select');
+    expect(sortSelect).toBeInTheDocument();
+  });
+
 });

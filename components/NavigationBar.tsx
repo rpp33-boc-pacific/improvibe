a6cdb6928f08bede08ff9f0277c99cacabe2bb61
jakobtruong@gmail.com <jakobtruong@gmail.com>
2022-06-03 20:27:36 -0700

    Refactored for home button to route to index instead of '/explorerpage'

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -216,7 +216,7 @@ export default function NavigationBar() {
           <HomeButton
             onClick={(event) => {
               event.preventDefault();
-              Router.push('/explorerpage')
+              Router.push('/')
             }}
           >
             improvibe

e796bb4b0fec219cc424ae459582f3de26d14a40
Joseph Casey <josephcasey@pop-os.localdomain>
2022-06-04 12:04:04 -0400

    Ability to adjust tempo and pitch independent of each other

--- components/projectEditor/Wave.tsx
+++ components/projectEditor/Wave.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
+import SoundTouch from './soundtouch.js';
 const { debounce } = require('lodash');
 
 const formWaveSurferOptions = (ref: any) => ({
@@ -20,6 +21,8 @@ interface Props {
   isPlaying: boolean,
 }
 
+let audioNode: any;
+
 const Wave: NextPage<Props> = ({ data, isPlaying }) => {
   const waveformRef: any = useRef();
   const wavesurfer: { current: any, [key: string]: any } = useRef();
@@ -36,16 +39,35 @@ const Wave: NextPage<Props> = ({ data, isPlaying }) => {
 
   const create = debounce(async () => {
     const WaveSurfer = await require('wavesurfer.js');
+    SoundTouch(window);
     const options = formWaveSurferOptions(waveformRef.current);
     wavesurfer.current = WaveSurfer.create(options);
     wavesurfer.current.load(data.trackAudio);
+
+    wavesurfer.current.on('ready', function() {
+      const soundTouchObj = new window.soundtouch.SoundTouch(44100);
+      soundTouchObj.tempo = data.tempo;
+      soundTouchObj.pitch = data.pitch;
+      wavesurfer.current.backend.source.buffer.extract = function(target: [any], numFrames: number, position: number) {
+        var l = wavesurfer.current.backend.source.buffer.getChannelData(0),
+          r = wavesurfer.current.backend.source.buffer.getChannelData(1);
+        for (var i = 0; i < numFrames; i++) {
+          target[i * 2] = l[i + position];
+          target[i * 2 + 1] = r[i + position];
+        }
+        return Math.min(numFrames, l.length - position);
+      };
+      const filter = new window.soundtouch.SimpleFilter(wavesurfer.current.backend.source.buffer, soundTouchObj);
+      audioNode = window.soundtouch.getWebAudioNode(wavesurfer.current.backend.ac, filter);
+      wavesurfer.current.backend.setFilter(audioNode);
+      wavesurfer.current.backend.filters[0].disconnect();
+    });
   });
 
   if (wavesurfer.current) {
     if (isPlaying) {
+      audioNode.connect(wavesurfer.current.backend.ac.destination);
       wavesurfer.current.setVolume(data.volume);
-      wavesurfer.current.setPlaybackRate(data.tempo);
-      wavesurfer.current.setPlaybackRate(data.tempo);
       wavesurfer.current.play(data.start, data.end);
     } else {
       wavesurfer.current.pause();

c0e68801fb08918b7a38dc5f2970a68a44e6927a
Joseph Casey <josephcasey@pop-os.localdomain>
2022-06-04 23:33:30 -0400

    FINALLY removed thumbs from mui sliders

--- components/projectEditor/SoundController.jsx
+++ components/projectEditor/SoundController.jsx
@@ -1,11 +1,18 @@
-import { Slider, Stack, ThemeProvider, createTheme, withStyles } from '@mui/material';
-import { NextPage } from 'next';
+import { Slider, Stack } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const CustomSlider = styled(Slider)({
+  "& .MuiSlider-thumb": {
+    width: '0px',
+    height: '0px',
+  },
+});
 
 const SoundController = ({ settings, changeSetting, isDisabled }) => {
   return (
     <Stack spacing={3} direction='row' justifyContent="space-between" sx={{ height: '2.8vh' }}>
       <div className='setting-label'>{settings.label}</div>
-      <Slider defaultValue={settings.value} min={settings.min} max={settings.max} onChange={changeSetting} valueLabelDisplay="auto" disabled={isDisabled} />
+      <CustomSlider defaultValue={settings.value} min={settings.min} max={settings.max} onChange={changeSetting} valueLabelDisplay="auto" disabled={isDisabled}/>
     </Stack>
   )
 };

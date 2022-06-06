60f50fc03b7d428b59e2a1d3a4564f193285c2d3
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-06-06 18:34:31 -0400

    Connected song tile to horizontalCarousel and updated user data to come from AppContext object and song data to come from API fetch

--- components/explorerPage/horizontalCarousel.tsx
+++ components/explorerPage/horizontalCarousel.tsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useState, useRef } from "react";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
+import AppContext from '../../AppContext';
 import '../../styles/Explorer.module.css';
 import "@splidejs/splide/dist/css/themes/splide-default.min.css";
-import Tile from './tile';
+import Tile from '../shared/SongTile';
 
 let uniqueKey = 0;
 
 const HorizontalCarousel = () => {
 
-  const initialItems = Array.apply(null, Array(6)).map(
-    (value, index) => index + 1
-  );
+  //I don't think this is necesary anymore?
+  // const initialItems = Array.apply(null, Array(6)).map(
+  //   (value, index) => index + 1
+  // );
 
   const ref = useRef();
-  const [items, setItems] = useState(initialItems);
+  const [songs, setSongs] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const user = useContext(AppContext); //the user will come from the AppContext
+
+  useEffect(() => {
+    // The songs will come from the api call
+    fetch('api/songs')
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      setSongs(result);
+      setLoading(false);
+    })
+    .catch((err) => {
+      console.log('Error:', err);
+    })
+  })
+
+
   return (
+    isLoading === true ? <>Loading...</> :
     <>
       {/* <hr /> */}
       <Splide
@@ -24,21 +46,21 @@ const HorizontalCarousel = () => {
           rewind: false,
           perPage: 4,
           perMove: 1,
-          gap: '1em',
-          padding: '1em',
-          border: '2em',
+          gap: '10em',
+          padding: '.5em',
+          // border: '2em',
           heightRatio: 0.25,
           pagination: false,
           width: '100%',
           backgroundColor: '#1976d2'
         }}
       >
-        {items.map(item => {
+        {songs.map(song => {
           return (
             <>
-            <SplideSlide key={item}>
+            <SplideSlide key={song}>
               <div className="slide" >
-                <Tile />
+                <Tile song={song} user={user} color={'white'}/>
               </div>
             </SplideSlide>
             </>

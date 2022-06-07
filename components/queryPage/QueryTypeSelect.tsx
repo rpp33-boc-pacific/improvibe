d5f65e9788dd2d1b735c1be9d062d6c8350df66c
jakobtruong@gmail.com <jakobtruong@gmail.com>
2022-06-07 04:01:35 -0700

    Refactored for single value select

---
+++ components/queryPage/QueryTypeSelect.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+export default function QueryTypeSelect({queryTypeParam, setQueryTypeParam}: any) {
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setQueryTypeParam(event.target.value as string);
+  };
+
+  return (
+    <Box role='query-type-select'>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="query-type-simple-select-label">Song Sort</InputLabel>
+        <Select
+          id="query-type-simple-select"
+          value={queryTypeParam}
+          label="Query Type Parameter"
+          onChange={handleChange}
+        >
+          <MenuItem value={'Songs'}>Songs</MenuItem>
+          <MenuItem value={'Artists'}>Artists</MenuItem>
+          <MenuItem value={'Genres'}>Genres</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}

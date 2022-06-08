cf84ed4576242daa0e045780f471259c7055bfbb
J.P. Doyle <jp.hackreactor@gmail.com>
2022-06-08 13:35:07 -0700

    Deleted config.js and updated next.config.js

--- config.ts
+++
@@ -1,3 +0,0 @@
-const IMG_KEY: string = '5f8e6e8e5a06455296bb232ce37227c4';
-
-export default IMG_KEY;
--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    IMG_KEY: '16a3cc0341c64b0f802243641da72f9b'
-  },
   reactStrictMode: true,
   images: {
     domains: [
       'artscimedia.case.edu',
-      'i.ibb.co',
-      'image.shutterstock.com',
-      'ychef.files.bbci.co.uk',
     ],
   }
 }

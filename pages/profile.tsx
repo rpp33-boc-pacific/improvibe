c0f758077c9acea05a562092d66aa9e32a665c65
J.P. Doyle <jp.hackreactor@gmail.com>
2022-06-08 10:15:49 -0700

    Updated profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,9 +1,6 @@
 import { useContext } from 'react';
-import Context from '../AppContext';
-import Image from 'next/image';
 import Link from 'next/link';
 import { NextPage } from 'next';
-import useSWR from 'swr';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
@@ -11,84 +8,106 @@ import Typography from '@mui/material/Typography';
 import { Stack } from '@mui/material';
 import NavigationBar from '../components/NavigationBar';
 import SongTile from '../components/shared/SongTile';
+import Context from '../AppContext';
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const tempContext = {
+  user: {
+    userId: '1',
+    name: 'David Bowe',
+    aboutMe: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+    deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+    deserunt mollit anim id est laborum.`,
+    photoUrl: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
+  },
+  songs: [
+    {
+      song_id: 1,
+      song_name: 'Song Name1',
+      artist_name: 'Artist Name1',
+      in_projects: false,
+      genre: 'rock',
+      photo_url: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
+      song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
+      liked: true,
+      public: true,
+    },
+    {
+      song_id: 2,
+      song_name: 'Song Name2',
+      artist_name: 'Artist Name2',
+      in_projects: true,
+      genre: 'hip hop',
+      photo_url: 'https://footdistrict.com/media/magefan_blog/footdistrict-run-dmc-adidas-union-historica-3-1.jpg',
+      song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
+      liked: true,
+      public: false,
+    },
+  ],
+};
 
 const Profile: NextPage = () => {
 
   const context: any = useContext(Context);
-  const userId = context.user;
-  const songs = context.songs;
 
-  const { data, error } = useSWR(`/api/profiles/${userId}?owner=true`, fetcher);
-
-  if (error) {
-    return (
-      <div>
-        <p>An error has occurred</p>
-      </div>
-    );
-  } else if (!data) {
-    return (
-      <div>
-        <p>Loading...</p>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <NavigationBar />
-        <Grid container spacing={1}>
-          <Grid item xs={4}>
+  return (
+    <div>
+      <NavigationBar />
+      <Grid container spacing={1}>
+        <Grid item xs={4}>
+          <Box>
+            <Container>
+              <img
+                alt='Profile picture of the artist'
+                src={tempContext.user.photoUrl}
+                height={300}
+                width={300}
+                style={{ borderRadius: '90%' }}
+                data-testid='picture'
+              />
+            </Container>
+          </Box>
+        </Grid>
+        <Grid container item xs={8}>
+          <Grid item xs={9}>
             <Box>
-              <Container>
-                <Image
-                  alt='Profile picture of the arist'
-                  src={data.photoUrl}
-                  height={300}
-                  width={300}
-                  style={{ borderRadius: '90%' }}
-                  data-testid='picture'
-                />
-              </Container>
+              <Typography>{tempContext.user.name}</Typography>
             </Box>
           </Grid>
-          <Grid container item xs={8}>
-            <Grid item xs={9}>
-              <Box>
-                <Typography>{data.name}</Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={3}>
-              <Box>
-                <Link href={'/profile-editor'}><Typography><a>Edit Profile</a></Typography></Link>
-              </Box>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography>About Me</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Box>
-                {data.aboutMe}
-              </Box>
-            </Grid>
+          <Grid item xs={3}>
+            <Box>
+              <Link href={'/profile-editor'}><Typography><a>Edit Profile</a></Typography></Link>
+            </Box>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={12}>
+            <Typography>About Me</Typography>
           </Grid>
-          <Grid container item xs={8}>
-            <Grid item xs={12}>
-              <Typography>My Songs</Typography>
-            </Grid>
-            <Grid container item xs={12}>
-              <Stack direction='row' spacing={1}>
-                {data.songs.map((song: any, index: number) => <Grid item key={index}><SongTile key={index} song={song} user={userId} color='white' /></Grid>)}
-              </Stack>
-            </Grid>
+          <Grid item xs={12}>
+            <Box>
+              {tempContext.user.aboutMe}
+            </Box>
           </Grid>
         </Grid>
-      </div>
-    );
-  }
+        <Grid item xs={4}>
+        </Grid>
+        <Grid container item xs={8}>
+          <Grid item xs={12}>
+            <Typography>My Songs</Typography>
+          </Grid>
+          <Grid container item xs={12}>
+            <Stack direction='row' spacing={1}>
+              {tempContext.songs.map((song: any, index: number) => <Grid item key={index}><SongTile key={index} song={song} user={tempContext.user.userId} color='white' /></Grid>)}
+            </Stack>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Profile;

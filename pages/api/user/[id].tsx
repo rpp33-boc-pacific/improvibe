5444227c0372bd24cce83ef85bd608ec3e7136d4
J.P. Doyle <jp.hackreactor@gmail.com>
2022-06-08 10:19:33 -0700

    Re-factored /user API

--- pages/api/user/[id].tsx
+++ pages/api/user/[id].tsx
@@ -1,38 +1,41 @@
 import client from '../../../sql/db';
 
+const profileExample = {
+  // name, aboutMe, photoUrl, and all songs (public and private)
+  userId: 1,
+  name: 'David Bowe',
+  aboutMe: 'All about David Bowe...',
+  photoUrl: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
+  songs: [
+    {
+      song_id: 1,
+      song_name: 'Song Name1',
+      artist_name: 'Artist Name1',
+      in_projects: false,
+      genre: 'rock',
+      photo_url: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
+      song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
+      liked: true,
+    },
+    {
+      song_id: 2,
+      song_name: 'Song Name2',
+      artist_name: 'Artist Name2',
+      in_projects: true,
+      genre: 'hip hop',
+      photo_url: 'https://footdistrict.com/media/magefan_blog/footdistrict-run-dmc-adidas-union-historica-3-1.jpg',
+      song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
+      liked: true
+    },
+  ]
+};
+
 export default function getUser(req: any, res: any) {
-  const userId = req.query.id
+  const userId = req.query.id;
+
   //Select id, artist, song, searched, photo url from users where id = userId
   //Select * from songs table where userId = userId
   //For each song add whether userId has liked this song and the genre associated with the genre_id and all the likes by all the users
 
-  let example = {
-    id: 9,
-    artist: 'David Bowe',
-    aboutMe: 'About David Bowe',
-    photoUrl: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
-    songs: [
-      {
-        id: 1,
-        name: 'Space Odity',
-        songPath: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-16.mp3',
-        totalLikes: 14,
-        liked: true,
-        genre: 'Rock',
-        artistPic: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
-        artist: 'David Bowe'
-      },
-      {
-      id: 2,
-      name: 'Golden Years',
-      songPath: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-17.mp3',
-      totalLikes: 21,
-      liked: false,
-      genre: 'Smooth Rock',
-      artistPic: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
-      artist: 'David Bowe'
-      }
-    ]
-  }
-  res.send(example)
+  res.send(profileExample);
 }

fde12e00bb2800e8cf96f68564746b8dbcad838b
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-06-09 17:58:06 -0400

    Player is not playing

--- components/shared/AudioPlayer.tsx
+++ components/shared/AudioPlayer.tsx
@@ -4,7 +4,6 @@ import AddToProjects from './AddToProjects';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
-// import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image';
 import Link from '@mui/material/Link';
@@ -13,7 +12,6 @@ import CloseIcon from '@mui/icons-material/Close';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import IconButton from '@mui/material/IconButton';
 import { borderRadius } from '@mui/system';
-// import Player from 'material-ui-audio-player';
 import Player from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 
@@ -28,30 +26,17 @@ const AudioPlayer = ({ song, user, color }: any) => {
     p: 4,
   };
 
-  // const [navigator, setNavigator] = useState({});
-  // const [isSet, set] = useState(false);
-
-  // useEffect(() => {
-  //   // setNavigator(navigator);
-  //   set(true);
-  //   console.log(window);
-  //   console.log(navigator);
-  // }, []);
-
   const ProfileImage = () => {
     return (
       <Image src={song.photo_url} alt="artist-profile-picture" layout={"fixed"} width="125px" height="125px"/>
       )
     }
 
-  // const muiTheme = createMuiTheme({});
-
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
     return (
-      // isSet === false ? <></> :
       <div id="play-song-container">
         <IconButton
           aria-label="open-player-modal"
@@ -66,7 +51,9 @@ const AudioPlayer = ({ song, user, color }: any) => {
             <Grid container direction="row" spacing={2} sx={{paddingBottom: '.5em'}}>
               <Grid item xs={1.5}>
               <Link href="./profile">
-                <ProfileImage></ProfileImage>
+                <div style={{borderRadius:"8px", overflow: "hidden", width:"125px"}}>
+                  <ProfileImage></ProfileImage>
+                </div>
               </Link>
               </Grid>
               <Grid item xs={3} sx={{paddingBotton: ".5em"}}>
@@ -84,12 +71,8 @@ const AudioPlayer = ({ song, user, color }: any) => {
                 </Grid>
               </Grid>
             </Grid>
-            {/* <audio role="audio-player" controls src={song.song_path}></audio> */}
-            <div style={{width: '100%'}}>
-              {/* <ThemeProvider theme={muiTheme}> */}
-                <Player src={song.song_path} style={{color: 'white', backgroundColor: '#333', border:'none'}}/>
-              {/* </ThemeProvider> */}
-            </div>
+              <Player src={song.song_path}  style={{boxShadow: 'none', color: 'white', backgroundColor: '#333', border:'none'}}/>
+              {/* <audio controls></audio> */}
             <IconButton
               aria-label="close-player-modal"
               sx={{color: 'white', position: 'absolute', top: '2%', right: '1%'}}

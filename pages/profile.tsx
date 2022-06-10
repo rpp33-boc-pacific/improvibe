85a956e05bb028d73c34d1d67d14c2ac65235dcd
J.P. Doyle <jp.hackreactor@gmail.com>
2022-06-10 16:59:09 -0700

    Switched to real context and ensured profile photo renders

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -11,50 +11,55 @@ import NavigationBar from '../components/NavigationBar';
 import SongTile from '../components/shared/SongTile';
 import Context from '../AppContext';
 
-const tempContext = {
-  user: {
-    userId: '1',
-    name: 'David Bowe',
-    aboutMe: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-    deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-    deserunt mollit anim id est laborum.`,
-    photoUrl: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
-  },
-  songs: [
-    {
-      song_id: 1,
-      song_name: 'Song Name1',
-      artist_name: 'Artist Name1',
-      in_projects: false,
-      genre: 'rock',
-      photo_url: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
-      song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
-      liked: true,
-      public: true,
-    },
-    {
-      song_id: 2,
-      song_name: 'Song Name2',
-      artist_name: 'Artist Name2',
-      in_projects: true,
-      genre: 'hip hop',
-      photo_url: 'https://footdistrict.com/media/magefan_blog/footdistrict-run-dmc-adidas-union-historica-3-1.jpg',
-      song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
-      liked: true,
-      public: false,
-    },
-  ],
-};
+// const tempContext = {
+//   user: {
+//     userId: '1',
+//     name: 'David Bowe',
+//     aboutMe: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+//     labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+//     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+//     dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+//     deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+//     labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+//     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+//     dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+//     deserunt mollit anim id est laborum.`,
+//     photoUrl: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
+//   },
+//   songs: [
+//     {
+//       song_id: 1,
+//       song_name: 'Song Name1',
+//       artist_name: 'Artist Name1',
+//       in_projects: false,
+//       genre: 'rock',
+//       photo_url: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
+//       song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
+//       liked: true,
+//       public: true,
+//     },
+//     {
+//       song_id: 2,
+//       song_name: 'Song Name2',
+//       artist_name: 'Artist Name2',
+//       in_projects: true,
+//       genre: 'hip hop',
+//       photo_url: 'https://footdistrict.com/media/magefan_blog/footdistrict-run-dmc-adidas-union-historica-3-1.jpg',
+//       song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
+//       liked: true,
+//       public: false,
+//     },
+//   ],
+// };
 
 const Profile: NextPage = () => {
 
   const context: any = useContext(Context);
+  const userId = context.id;
+  const photoUrl = context.user.photoUrl;
+  const name = context.user.name;
+  const aboutMe = context.user.about_me;
+  const songs = context.songs;
 
   return (
     <div>
@@ -65,7 +70,7 @@ const Profile: NextPage = () => {
             <Box sx={{ marginLeft: '40px', marginTop: '40px' }}>
               <Image
                 alt='Profile picture of the artist'
-                src={tempContext.user.photoUrl}
+                src={photoUrl || 'https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif'}
                 height={300}
                 width={300}
                 style={{ borderRadius: '90%' }}
@@ -77,7 +82,7 @@ const Profile: NextPage = () => {
         <Grid container item xs={8} sx={{ marginTop: '40px' }}>
           <Grid item xs={9}>
             <Box>
-              <Typography>{tempContext.user.name}</Typography>
+              <Typography>{name}</Typography>
             </Box>
           </Grid>
           <Grid item xs={3}>
@@ -90,7 +95,7 @@ const Profile: NextPage = () => {
           </Grid>
           <Grid item xs={12} sx={{ marginRight: '60px' }}>
             <Box>
-              {tempContext.user.aboutMe}
+              {aboutMe}
             </Box>
           </Grid>
         </Grid>
@@ -102,7 +107,7 @@ const Profile: NextPage = () => {
           </Grid>
           <Grid container item xs={12}>
             <Stack direction='row' spacing={1.5}>
-              {tempContext.songs.map((song: any, index: number) => <Grid item key={index}><SongTile key={index} song={song} user={tempContext.user.userId} color='white' /></Grid>)}
+              {songs.map((song: any, index: number) => <Grid item key={index}><SongTile key={index} song={song} user={userId} color='white' /></Grid>)}
             </Stack>
           </Grid>
         </Grid>

cb4f1e8db675086e77c45dc207f6d4b85e5f72c2
jakobtruong@gmail.com <jakobtruong@gmail.com>
2022-06-11 07:07:51 -0700

    Added additional styling and rendering data from database

--- pages/query/[queryString].tsx
+++ pages/query/[queryString].tsx
@@ -10,6 +10,7 @@ import SongResult from '../../components/queryPage/SongResult';
 import ArtistResult from '../../components/queryPage/ArtistResult';
 import QueryTypeSelect from '../../components/queryPage/QueryTypeSelect';
 import SortSelect from '../../components/queryPage/SortSelect';
+import Typography from '@mui/material/Typography';
 import pool from '../../sql/db';
 import CardContent from '@mui/material/CardContent';
 import useQuery from '../../helper/query';
@@ -18,24 +19,22 @@ const Query: NextPage = () => {
 
   const Item = styled('div')(({ theme }) => ({
     ...theme.typography.body2,
-    // padding: theme.spacing(1),
-    // textAlign: 'center',
     marginLeft: '200px',
     color: theme.palette.text.secondary,
   }));
 
   const userProp = {
-    userId: 1,
+    id: 1,
     name: 'Artist Name',
     email: 'artistname@email.com',
     about_me: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    liked: false //liked by current user
+    liked: false, //liked by current user
+    photo_url: "https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif",
   }
-  // `SELECT projects.id, projects.name, projects.genre, projects.likes, projects.shares, projects.user_id, projects.song_path, projects.date_created, users.photo_url
   const songProp = {
     id: 1,
-    name: 'Song Name',
-    artistName: 'Artist Name',
+    song_name: 'Song Name',
+    artist_name: 'Artist Name',
     song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
     genre: 'Rock',
     photo_url: "https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif",
@@ -51,6 +50,8 @@ const Query: NextPage = () => {
   var [menuItems, setMenuItems] = useState(['Most Liked', 'Least Liked', 'Most Shared', 'Least Shared', 'Most Recent', 'Least Recent']);
   const queryInput = useQuery() || {queryString: ''};
   var [queryState, setQueryState] = useState(0);
+  var searchResults;
+  var searchHeader;
 
   useEffect(() => {
     if (queryTypeParam === 'Songs') {
@@ -94,7 +95,6 @@ const Query: NextPage = () => {
     console.log(queryTypeParam);
     console.log(sortParam);
   }, [sortParam, queryState]);
-
   return (
     <div>
       <Head>
@@ -102,15 +102,11 @@ const Query: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavigationBar/>
-      <Grid container spacing={1} sx={{padding: 0}}>
-          <Grid item xs={12} sx={{padding: 0}}>
-            <CardContent sx={{display: 'flex', flexDirection: 'row-reverse', mr: "200px", padding:0}}>
+      <CardContent sx={{display: 'flex', flexDirection: 'row-reverse', mr: "200px", padding:0}}>
               <SortSelect sortParam={sortParam} setSortParam={setSortParam} menuItems={menuItems}/>
               <QueryTypeSelect queryTypeParam={queryTypeParam} setQueryTypeParam={setQueryTypeParam}/>
-            </CardContent>
-          </Grid>
-        </Grid>
-      {loadedData[0] === 'Not Loadeds' ?
+      </CardContent>
+      {loadedData[0] === 'Not Loaded' ?
       <Grid container spacing={1} direction="column">
         <Grid item xs={12}>
           <Skeleton variant="text" animation="wave" height={30} width="25%" sx={{ margin: 0, ml: '100px'}}/>
@@ -137,14 +133,24 @@ const Query: NextPage = () => {
           <Item>Showing {loadedData.length} results</Item>
         </Grid>
         {/* <Grid item xs={12}>
-          <ArtistResult user={userProp}/>
+            <ArtistResult user={userProp}/>
+        </Grid>
+        <Grid item xs={12}>
+            <SongResult song={songProp} user={userProp}/>
         </Grid> */}
-        <Grid item xs={12}>
-          <SongResult song={songProp} user={userProp}/>
-        </Grid>
-        <Grid item xs={12}>
-          <SongResult song={songProp} user={userProp}/>
-        </Grid>
+        {queryTypeParam === 'Artists' ?
+          loadedData.map((item: any) => {
+            return <Grid item xs={12}>
+            <ArtistResult user={item}/>
+          </Grid>;
+          })
+          :
+          loadedData.map((item: any) => {
+            return <Grid item xs={12}>
+            <SongResult song={item} user={{user_id: item.user_id}}/>
+          </Grid>;
+          })
+        }
       </Grid>
       }
     </div>

fc33d5ece0034225f9aaaf9bc89b780467f7ca88
jakobtruong@gmail.com <jakobtruong@gmail.com>
2022-06-11 07:05:31 -0700

    Added additional styling and now passing down data through props

--- components/queryPage/SongResult.tsx
+++ components/queryPage/SongResult.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { useContext, createContext, useState } from 'react';
+import SongTileContext from '../shared/SongTileContext';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -7,18 +9,22 @@ import Image from 'next/image';
 import Typography from '@mui/material/Typography';
 import LikeButton from '../shared/LikeButton';
 import { styled } from '@mui/material/styles';
+import Link from '@mui/material/Link';
 import { containerClasses } from '@mui/system';
-import Player from '../shared/AudioPlayer';
+import Player from '../shared/AudioPlayer.jsx';
 import ShareButton from './ShareButton';
 
 export default function SongResult({ song, user }: any) {
   const PlayerContainer = styled('div')(({ theme }) => ({
-    // top: '50px',
-    // paddingLeft: "100px"
+    paddingTop: "50px",
+    paddingLeft: "20px",
+    marginTop: 0,
+    position: "absolute"
   }));
   const GenreContainer = styled('div')(({ theme }) => ({
-    width: "100px",
     margin: "auto",
+    marginRight: "20px",
+    marginTop: 0,
     float: "none",
     overflow: "hidden",
     textAlign: "right"
@@ -29,32 +35,38 @@ export default function SongResult({ song, user }: any) {
     height: '85px',
     borderRadius: '10%'
   }));
+  const artistProfileReference = `../profile/${song.user_id}`;
+
+  const [liked, setLiked] = useState(song.liked);
+  const [likes, setLikes] = useState(song.cumulative_likes);
 
   return (
-    <Card sx={{border: 1, margin: 0, padding: 0, borderColor: 'grey.500', ml: '200px', mr: '200px'}} role='song-search-result'>
-      <CardContent sx={{display: 'flex', flexDirection: 'row', pb: 0}}>
-        <ArtistImage src="https://image.shutterstock.com/image-photo/linked-blocks-bank-world-currencies-600w-1926421151.jpg" alt='https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif'/>
-        <CardContent sx={{display: 'flex', flexDirection: 'column', pb: 0, margin: 0, padding: 0, width: "400px"}}>
-          <Typography gutterBottom variant="h5" sx={{ mt: 0, ml: "30px", mb: 0, flexShrink:0}}>
-            {song.songName}
-          </Typography>
-          <Typography variant="body1" color="text.secondary" sx={{ml: "30px"}}>
-            {song.artistName}
-          </Typography>
+    <SongTileContext.Provider value={{liked, setLiked, likes, setLikes}}>
+      <Card sx={{border: 1, margin: 0, padding: 0, borderColor: 'grey.500', ml: '200px', mr: '200px', height: '160px'}} role='song-search-result'>
+        <CardContent sx={{display: 'flex', flexDirection: 'row', pb: 0}}>
+          <ArtistImage src={song.photo_url} alt='https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif'/>
+          <CardContent sx={{display: 'flex', flexDirection: 'column', pb: 0, margin: 0, padding: 0, width: "400px"}}>
+            <Typography gutterBottom variant="h5" sx={{ mt: 0, ml: "30px", mb: 0, flexShrink:0}}>
+              {song.song_name}
+            </Typography>
+            <Link href={artistProfileReference} variant="body1" underline="hover" color="text.secondary" sx={{ml: "30px", cursor: "pointer"}}>
+              {song.artist_name}
+            </Link>
+            <PlayerContainer>
+            <Player song={song} user={user} color='red'/>
+          </PlayerContainer>
+          </CardContent>
+          <GenreContainer>
+            <Typography variant="body2" color="text.secondary">
+              Genre: {song.genre}
+            </Typography>
+          </GenreContainer>
         </CardContent>
-        <GenreContainer>
-          <Typography variant="body2" color="text.secondary" sx={{}}>
-            Genre: {song.genre}
-          </Typography>
-        </GenreContainer>
-      </CardContent>
-      <CardActions sx={{flexDirection: 'row-reverse', padding: 0, margin: 0, pr: 3}}>
-        <ShareButton songURL={song.song_path}/>
-        <LikeButton song={song} user={user} color='red'/>
-        <PlayerContainer>
-          <Player song={song} user={user} color='red'/>
-        </PlayerContainer>
-      </CardActions>
-    </Card>
+        <CardActions sx={{flexDirection: 'row-reverse', padding: 0, margin: 0, pr: 3, pb: 3}}>
+          <ShareButton songURL={song.song_path} shares={song.shares}/>
+          <LikeButton song={song} user={user} color='red'/>
+        </CardActions>
+      </Card>
+    </SongTileContext.Provider>
   );
 }

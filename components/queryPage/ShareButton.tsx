9114ea590614ef0cd74a461e0eea5c3c3072f73c
jakobtruong@gmail.com <jakobtruong@gmail.com>
2022-06-11 00:12:05 -0700

    Added initial Share Button modal

--- components/queryPage/ShareButton.tsx
+++ components/queryPage/ShareButton.tsx
@@ -3,6 +3,8 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import '../../styles/ShareButton.module.css';
+import LinkIcon from '@mui/icons-material/Link';
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -13,10 +15,10 @@ const style = {
   bgcolor: 'background.paper',
   border: '1px solid #000',
   boxShadow: 24,
-  p: 4,
+  p: 2,
 };
 
-export default function ShareButton() {
+export default function ShareButton(songURL: any) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -30,13 +32,15 @@ export default function ShareButton() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+        <Box sx={style} textAlign="center">
+          <Typography id="modal-modal-title" variant="h6" component="h2" sx={{margin: 0, padding: 0}}>
             Text in a modal
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <div className="field d-flex align-items-center justify-content-center">
+            <LinkIcon/>
+            <input type="text" value={songURL}/>
+            <Button>Copy</Button>
+          </div>
         </Box>
       </Modal>
     </div>

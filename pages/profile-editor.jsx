cc58b0fa837efa7b232f8d05925c428449d0cfc4
J.P. Doyle <jp.hackreactor@gmail.com>
2022-06-11 09:47:42 -0700

    Hope this works

--- pages/profile-editor.jsx
+++ pages/profile-editor.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import axios from 'axios';
@@ -10,34 +10,37 @@ import { Stack } from '@mui/material';
 import Photo from '../components/profileEditor/Photo';
 import NavigationBar from '../components/NavigationBar';
 import Song from '../components/profileEditor/Song';
-import Context from '../AppContext';
+import AppContext from '../AppContext';
+import { getSession } from 'next-auth/react';
+import useSWR from 'swr';
 
 const noPhotoUrl = 'https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif';
 
-const ProfileEditor: NextPage = () => {
+const ProfileEditor = (props) => {
 
   const router = useRouter();
-  const context: any = useContext(Context);
-  const id = context.user.id;
-  const songs = context.songs;
+  const { user, setUser } = useContext(AppContext);
+  setUser(props.user);
+  const userId = user.id;
 
-  const [ name, setName ] = useState(context.user.name);
-  const [ email, setEmail ] = useState(context.user.email);
-  const [ aboutMe, setAbout ] = useState(context.user.about_me);
-  const [ password, setPassword ] = useState('');
-  const [ photoUrl, setPhotoUrl ] = useState(context.user.photoUrl);
-  const [ saveFailed, setSaveFailed ] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [aboutMe, setAboutMe] = useState('');
+  const [password, setPassword] = useState('');
+  const [photoUrl, setPhotoUrl] = useState('');
+  const [songs, setSongs] = useState([]);
+  const [saveFailed, setSaveFailed] = useState(false);
 
-  const handleNameChange = (event: any) => setName(event.target.value);
-  const handleEmailChange = (event: any) => setEmail(event.target.value);
-  const handleAboutMeChange = (event: any) => setAbout(event.target.value);
-  const handlePasswordChange = (event: any) => setPassword(event.target.value);
-  const handlePhotoUrlChange = (url: string) => setPhotoUrl(url);
+  const handleNameChange = (event) => setName(event.target.value);
+  const handleEmailChange = (event) => setEmail(event.target.value);
+  const handleAboutMeChange = (event) => setAboutMe(event.target.value);
+  const handlePasswordChange = (event) => setPassword(event.target.value);
+  const handlePhotoUrlChange = (url) => setPhotoUrl(url);
   const handleSave = async () => {
     let err = false;
     try {
       await axios.put('/api/user/update', {
-        id,
+        id: userId,
         name,
         email,
         password,
@@ -55,6 +58,20 @@ const ProfileEditor: NextPage = () => {
     }
   };
 
+  useEffect(() => {
+    axios.get(`/api/user/${userId}`)
+      .then((res) => {
+        setName(res.data.user.name);
+        setEmail(res.data.user.email);
+        setAboutMe(res.data.user.about_me);
+        setPhotoUrl(res.data.user.photoUrl);
+        setSongs(res.data.songs);
+      })
+      .catch((err) => {
+        console.log('Error getting user info');
+      })
+  }, []);
+
   if (!saveFailed) {
     return (
       <div>
@@ -62,7 +79,7 @@ const ProfileEditor: NextPage = () => {
         <Grid container spacing={1}>
           <Grid item xs={3}>
             <Box sx={{ marginLeft: '60px', marginTop: '40px' }}>
-              <Photo photoUrl={photoUrl || noPhotoUrl } handlePhotoUrlChange={handlePhotoUrlChange} />
+              <Photo photoUrl={photoUrl || noPhotoUrl} handlePhotoUrlChange={handlePhotoUrlChange} />
             </Box>
           </Grid>
           <Grid container item xs={9} sx={{ paddingRight: '60px', marginTop: '40px' }}>
@@ -78,9 +95,9 @@ const ProfileEditor: NextPage = () => {
                 <TextField type='email' size={'small'} value={email} onChange={handleEmailChange} />
                 <TextField type='password' size={'small'} placeholder='New password' value={password} onChange={handlePasswordChange} />
                 <TextField type='text' size={'small'} multiline value={aboutMe} onChange={handleAboutMeChange} />
-                <Typography sx={{ fontSize: '2vh', fontWeight: 'bold' }}>Songs ({context.songs.length})</Typography>
+                <Typography sx={{ fontSize: '2vh', fontWeight: 'bold' }}>Songs ({songs.length})</Typography>
                 <Stack>
-                  {songs.map((song: any, index: number) => <Song key={index} song={song} />)}
+                  {songs.map((song, index) => <Song key={index} song={song} />)}
                 </Stack>
               </Stack>
             </Grid>
@@ -95,7 +112,7 @@ const ProfileEditor: NextPage = () => {
         <Grid container spacing={1}>
           <Grid item xs={3}>
             <Box sx={{ marginLeft: '60px', marginTop: '40px' }}>
-              <Photo photoUrl={photoUrl || noPhotoUrl } handlePhotoUrlChange={handlePhotoUrlChange} />
+              <Photo photoUrl={photoUrl || noPhotoUrl} handlePhotoUrlChange={handlePhotoUrlChange} />
             </Box>
           </Grid>
           <Grid container item xs={9} sx={{ paddingRight: '60px', marginTop: '40px' }}>
@@ -111,9 +128,9 @@ const ProfileEditor: NextPage = () => {
                 <TextField type='email' size={'small'} value={email} onChange={handleEmailChange} />
                 <TextField type='password' size={'small'} placeholder='New password' value={password} onChange={handlePasswordChange} />
                 <TextField type='text' size={'small'} multiline value={aboutMe} onChange={handleAboutMeChange} />
-                <Typography sx={{ fontSize: '2vh', fontWeight: 'bold' }}>Songs ({context.songs.length})</Typography>
+                <Typography sx={{ fontSize: '2vh', fontWeight: 'bold' }}>Songs ({songs.length})</Typography>
                 <Stack>
-                  {songs.map((song: any, index: number) => <Song key={index} song={song} />)}
+                  {songs.map((song, index) => <Song key={index} song={song} />)}
                 </Stack>
               </Stack>
             </Grid>
@@ -122,6 +139,19 @@ const ProfileEditor: NextPage = () => {
       </div>
     );
   }
+};
+
+export async function getServerSideProps(AppContext) {
+  const { req, res } = AppContext;
+  const session = await getSession({ req });
+  if (!session) {
+    res.writeHead(302, {
+      Location: "/logIn",
+    });
+    res.end();
+    return;
+  }
+  return { props: session };
 };
 
 export default ProfileEditor;

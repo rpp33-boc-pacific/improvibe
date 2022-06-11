276b21d35e70099e51bddad00e3d842e59b1f7cf
J.P. Doyle <jp.hackreactor@gmail.com>
2022-06-10 22:00:19 -0700

    Updated profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,143 +3,23 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { NextPage } from 'next';
 import Box from '@mui/material/Box';
-import { flexbox } from '@mui/system';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { Stack } from '@mui/material';
 import NavigationBar from '../components/NavigationBar';
 import SongTile from '../components/shared/SongTile';
 import Context from '../AppContext';
-import styles from '../../styles/Profile.module.css';
 
-// const tempContext = {
-//   user: {
-//     userId: '1',
-//     name: 'David Bowe',
-//     aboutMe: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-//     labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-//     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-//     dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-//     deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-//     labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-//     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-//     dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-//     deserunt mollit anim id est laborum.`,
-//     photoUrl: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
-//   },
-//   songs: [
-//     {
-//       song_id: 1,
-//       song_name: 'Song Name1',
-//       artist_name: 'Artist Name1',
-//       in_projects: false,
-//       genre: 'rock',
-//       photo_url: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
-//       song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
-//       liked: true,
-//       public: true,
-//     },
-//     {
-//       song_id: 2,
-//       song_name: 'Song Name2',
-//       artist_name: 'Artist Name2',
-//       in_projects: true,
-//       genre: 'hip hop',
-//       photo_url: 'https://footdistrict.com/media/magefan_blog/footdistrict-run-dmc-adidas-union-historica-3-1.jpg',
-//       song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
-//       liked: true,
-//       public: false,
-//     },
-//     {
-//       song_id: 1,
-//       song_name: 'Song Name1',
-//       artist_name: 'Artist Name1',
-//       in_projects: false,
-//       genre: 'rock',
-//       photo_url: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
-//       song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
-//       liked: true,
-//       public: true,
-//     },
-//     {
-//       song_id: 2,
-//       song_name: 'Song Name2',
-//       artist_name: 'Artist Name2',
-//       in_projects: true,
-//       genre: 'hip hop',
-//       photo_url: 'https://footdistrict.com/media/magefan_blog/footdistrict-run-dmc-adidas-union-historica-3-1.jpg',
-//       song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
-//       liked: true,
-//       public: false,
-//     },
-//     {
-//       song_id: 1,
-//       song_name: 'Song Name1',
-//       artist_name: 'Artist Name1',
-//       in_projects: false,
-//       genre: 'rock',
-//       photo_url: 'https://ychef.files.bbci.co.uk/976x549/p01j3jyb.jpg',
-//       song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
-//       liked: true,
-//       public: true,
-//     },
-//     {
-//       song_id: 2,
-//       song_name: 'Song Name2',
-//       artist_name: 'Artist Name2',
-//       in_projects: true,
-//       genre: 'hip hop',
-//       photo_url: 'https://footdistrict.com/media/magefan_blog/footdistrict-run-dmc-adidas-union-historica-3-1.jpg',
-//       song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
-//       liked: true,
-//       public: false,
-//     },
-//     {
-//       song_id: 2,
-//       song_name: 'Song Name2',
-//       artist_name: 'Artist Name2',
-//       in_projects: true,
-//       genre: 'hip hop',
-//       photo_url: 'https://footdistrict.com/media/magefan_blog/footdistrict-run-dmc-adidas-union-historica-3-1.jpg',
-//       song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
-//       liked: true,
-//       public: false,
-//     },
-//     {
-//       song_id: 2,
-//       song_name: 'Song Name2',
-//       artist_name: 'Artist Name2',
-//       in_projects: true,
-//       genre: 'hip hop',
-//       photo_url: 'https://footdistrict.com/media/magefan_blog/footdistrict-run-dmc-adidas-union-historica-3-1.jpg',
-//       song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
-//       liked: true,
-//       public: false,
-//     },
-//     {
-//       song_id: 2,
-//       song_name: 'Song Name2',
-//       artist_name: 'Artist Name2',
-//       in_projects: true,
-//       genre: 'hip hop',
-//       photo_url: 'https://footdistrict.com/media/magefan_blog/footdistrict-run-dmc-adidas-union-historica-3-1.jpg',
-//       song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
-//       liked: true,
-//       public: false,
-//     },
-//   ],
-// };
 const noPhotoUrl = 'https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif';
 
 const Profile: NextPage = () => {
 
   const context: any = useContext(Context);
-  const userId = context.id || 1;
+  const userId = context.id;
   const photoUrl = context.user.photoUrl || noPhotoUrl;
-  const name = context.user.name || 'John Doe';
-  const aboutMe = context.user.about_me || 'About me lorem ipsum lorem ipsum ad nauseaum';
-  const songs = context.songs || [];
+  const name = context.user.name;
+  const aboutMe = context.user.about_me;
+  const songs = context.songs;
 
   return (
     <div>

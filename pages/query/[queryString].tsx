aef7c7c5e9383d085166d9f23a37add6f9d24ab1
jakobtruong@gmail.com <jakobtruong@gmail.com>
2022-06-11 01:07:30 -0700

    Refactored state management and data passing through components

--- pages/query/[queryString].tsx
+++ pages/query/[queryString].tsx
@@ -31,14 +31,18 @@ const Query: NextPage = () => {
     about_me: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     liked: false //liked by current user
   }
+  // `SELECT projects.id, projects.name, projects.genre, projects.likes, projects.shares, projects.user_id, projects.song_path, projects.date_created, users.photo_url
   const songProp = {
-    songName: 'Song Name',
+    id: 1,
+    name: 'Song Name',
     artistName: 'Artist Name',
-    songPath: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
+    song_path: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
     genre: 'Rock',
-    tags: ['smooth', 'funky'],
-    artistPic: "https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif",
-    cumulativeLikes: 234,
+    photo_url: "https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif",
+    likes: 234,
+    shares: 100,
+    user_id: 1,
+    date_created: 1
   }
 
   var [loadedData, setLoadedData] = useState(['Not Loaded']);
@@ -52,19 +56,24 @@ const Query: NextPage = () => {
     if (queryTypeParam === 'Songs') {
       setMenuItems(['Most Liked', 'Least Liked', 'Most Shared', 'Least Shared', 'Most Recent', 'Least Recent']);
       setSortParam('Most Liked');
+      setQueryState(1);
     } else if (queryTypeParam === 'Artists') {
       setMenuItems(['Most Popular', 'Least Popular', 'Alphabetical']);
       setSortParam('Most Popular');
+      setQueryState(1);
     } else if (queryTypeParam === 'Genres') {
       setMenuItems(['Most Liked', 'Least Liked', 'Most Shared', 'Least Shared', 'Most Recent', 'Least Recent', 'Alphabetical']);
       setSortParam('Most Liked');
+      setQueryState(1);
     }
   }, [queryTypeParam]);
+
   useEffect(() => {
     if (queryInput.queryString !== '') {
       setQueryState(1);
     }
   }, [queryInput]);
+
   useEffect(() => {
     setLoadedData(['Not Loaded']);
     setQueryState(0);
@@ -72,9 +81,10 @@ const Query: NextPage = () => {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      setLoadedData(data);
+      setLoadedData(data.rows);
     })
     .catch((err) => {
+      setLoadedData(['Error']);
       console.log('Error with GET request: ', err);
     });
     if (!queryInput) {
@@ -100,7 +110,7 @@ const Query: NextPage = () => {
             </CardContent>
           </Grid>
         </Grid>
-      {loadedData[0] === 'Not Loaded' ?
+      {loadedData[0] === 'Not Loadeds' ?
       <Grid container spacing={1} direction="column">
         <Grid item xs={12}>
           <Skeleton variant="text" animation="wave" height={30} width="25%" sx={{ margin: 0, ml: '100px'}}/>
@@ -124,7 +134,7 @@ const Query: NextPage = () => {
       :
       <Grid container spacing={1} direction="column">
         <Grid item xs={12}>
-          <Item>Showing 3 results</Item>
+          <Item>Showing {loadedData.length} results</Item>
         </Grid>
         {/* <Grid item xs={12}>
           <ArtistResult user={userProp}/>
